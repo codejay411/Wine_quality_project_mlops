824d9308fb3aac426fae7973310f9d4e61eaffb9
codejay411 <jaypr202@gmail.com>
2021-05-25 16:09:38 +0530

    setup done

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src", 
+    version="0.0.1",
+    description="Its a wine quality test machine learning project using mlops",
+    author="codejay411",
+    packages=find_packages(),
+    license ="MIT"
+)
